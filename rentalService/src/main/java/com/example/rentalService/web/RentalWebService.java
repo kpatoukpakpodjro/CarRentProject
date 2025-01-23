ee70be4a0a66a87d4b01aad7f98e86691cef7395
KPATOUKPA Kpodjro <kpatoukpakpodjro@gmail.com>
2025-01-23 15:47:39 +0100

    update of rentalService and actions

--- rentalService/src/main/java/com/example/rentalService/web/RentalWebService.java
+++ rentalService/src/main/java/com/example/rentalService/web/RentalWebService.java
@@ -25,7 +25,9 @@ public class RentalWebService {
         cars.add(car);
         car = new Car("44DD55", 5000);
         cars.add(car);
-        car = new Car("55DD66", 4500);
+        car = new Car("55EE66", 4500);
+        cars.add(car);
+        car = new Car("66FF77", 4500);
         cars.add(car);
     }
 
